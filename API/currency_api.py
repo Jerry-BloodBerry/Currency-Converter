624770d270fd3aa9c7bbe629e71e92da1afd88e5
Cezary Kurcewicz <czaru66@gmail.com>
2021-01-12 23:58:17 +0100

    made 2 error handling examples
    1. Wrong API
    2. Internet Connection is down

--- API/currency_api.py
+++ API/currency_api.py
@@ -2,23 +2,55 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+import urllib3
+
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY');
 
+
+def internet_on():
+    try:
+        http = urllib3.PoolManager()
+        r = http.request('GET', "https://www.google.com/")
+        #print(r.status)
+    except:
+        return False
+    
+
+
 def get_available_currencies():
-    """Returns currencies that are currently listed in API"""
-    global API_KEY
-    URL = f"http://data.fixer.io/api/symbols?access_key={API_KEY}"
-    result = requests.get(URL)
-    json_data = json.loads(result.text)
-    symbols_dict = json_data['symbols']
+    if internet_on() is False:
+        print("Brak polaczenia z internetem")
+        os.sys.exit()
+    try:
+        """Returns currencies that are currently listed in API"""
+        global API_KEY
+        URL = f"http://data.fixer.io/api/symbols?access_key={API_KEY}"
+        result = requests.get(URL)
+        json_data = json.loads(result.text)
+        symbols_dict = json_data['symbols']
+    except KeyError:
+        print("Niewlasciwy klucz API!") 
+        os.sys.exit()
+    except ConnectionError:
+        print("Blad polaczenia!") 
+        os.sys.exit()
     return symbols_dict
+        
 
 def get_latest_rates():
-    """Returns latest currency rates with base EUR and update timestamp"""
-    global API_KEY
-    URL = f"http://data.fixer.io/api/latest?access_key={API_KEY}"
-    result = requests.get(URL)
-    json_data = json.loads(result.text)
+    try:
+        """Returns latest currency rates with base EUR and update timestamp"""
+        global API_KEY
+        URL = f"http://data.fixer.io/api/latest?access_key={API_KEY}"
+        result = requests.get(URL)
+        json_data = json.loads(result.text)
+    except KeyError:
+        print("Niewlasciwy klucz API!") 
+        os.sys.exit()
+    except ConnectionError:
+        print("Blad polaczenia!") 
+        os.sys.exit()
     return json_data
+
---
+++ tescik.py
@@ -0,0 +1,2 @@
+from API import currency_api
+print(currency_api.get_available_currencies())
